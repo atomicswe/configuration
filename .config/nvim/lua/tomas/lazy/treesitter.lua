a5ca6124622c378d2b0d4d44e116e70c892e90de
tomassantunes <tomasbantunes@gmail.com>
2024-12-27 19:59:53 +0000

    blade tree sitter highlights

--- .config/nvim/lua/tomas/lazy/treesitter.lua
+++ .config/nvim/lua/tomas/lazy/treesitter.lua
@@ -42,5 +42,20 @@ return {
         }
 
         vim.treesitter.language.register("templ", "templ")
-    end
+
+        require("nvim-treesitter.parsers").get_parser_configs().blade = {
+            install_info = {
+                url = "https://github.com/EmranMR/tree-sitter-blade",
+                files = {"src/parser.c"},
+                branch = "main",
+            },
+            filetype = "blade"
+        }
+
+        vim.filetype.add({
+            pattern = {
+                ['.*%.blade%.php'] = 'blade',
+            }
+        })
+    end,
 }
