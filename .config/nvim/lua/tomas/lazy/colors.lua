7b27961a0260e58760d6db426367a256b0041ea3
tomassantunes <tomasbantunes@gmail.com>
2025-01-10 16:11:45 +0000

    modified ghostty and nvim

--- .config/nvim/lua/tomas/lazy/colors.lua
+++ .config/nvim/lua/tomas/lazy/colors.lua
@@ -76,7 +76,7 @@ return {
                 },
             })
 
-            ColorMyPencils("catppuccin")
+            -- ColorMyPencils("catppuccin")
         end
     },
 
@@ -88,6 +88,40 @@ return {
             vim.g.mellow_italic_comments = false
 
             -- ColorMyPencils("mellow")
+        end
+    },
+
+    {
+        "rebelot/kanagawa.nvim",
+        name = "kanagawa",
+        config = function ()
+            require('kanagawa').setup({
+                compile = false,             -- enable compiling the colorscheme
+                undercurl = true,            -- enable undercurls
+                commentStyle = { italic = true },
+                functionStyle = {},
+                keywordStyle = { italic = true},
+                statementStyle = { bold = true },
+                typeStyle = {},
+                transparent = false,         -- do not set background color
+                dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+                terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+                colors = {                   -- add/modify theme and palette colors
+                    palette = {},
+                    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+                },
+                overrides = function(colors) -- add/modify highlights
+                    return {}
+                end,
+                theme = "wave",              -- Load "wave" theme when 'background' option is not set
+                background = {               -- map the value of 'background' option to a theme
+                    dark = "wave",           -- try "dragon" !
+                    light = "lotus"
+                },
+            })
+
+            ColorMyPencils("kanagawa")
+
         end
     }
 }
