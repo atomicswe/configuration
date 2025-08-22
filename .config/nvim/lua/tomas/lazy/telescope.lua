411ecf600cb1ab914227e2a862d7fd745434dc37
atomicswe <tomasbantunes@gmail.com>
2025-08-22 19:02:06 +0100

    update nvim and work.dotfiles

--- .config/nvim/lua/tomas/lazy/telescope.lua
+++ .config/nvim/lua/tomas/lazy/telescope.lua
@@ -24,6 +24,7 @@ return {
         vim.keymap.set('n', '<leader>pf', builtin.find_files, opts)
         vim.keymap.set('n', '<C-p>', builtin.git_files, opts)
         vim.keymap.set('n', '<leader>pws', builtin.live_grep, opts)
+        vim.keymap.set('n', '<leader>ps', builtin.grep_string, opts)
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end
 }
