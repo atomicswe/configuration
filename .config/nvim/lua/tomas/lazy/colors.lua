d2f967d1a696bc6eca53adc2dd6b3f8d43889b5b
atomicswe <tomasbantunes@gmail.com>
2025-11-09 22:42:07 +0000

    new theme

--- .config/nvim/lua/tomas/lazy/colors.lua
+++ .config/nvim/lua/tomas/lazy/colors.lua
@@ -4,7 +4,6 @@ function ColorMyPencils(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
 end
 
 return {
@@ -19,33 +18,33 @@ return {
                 },
             })
 
-            ColorMyPencils()
+            -- ColorMyPencils()
         end
     },
 
     {
         "catppuccin/nvim",
         name = "catppuccin",
-        config = function ()
+        config = function()
             require("catppuccin").setup({
                 flavour = "auto", -- latte, frappe, macchiato, mocha
-                background = { -- :h background
+                background = {    -- :h background
                     light = "latte",
                     dark = "mocha",
                 },
                 transparent_background = false, -- disables setting the background color.
-                show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-                term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+                show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+                term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
                 dim_inactive = {
-                    enabled = false, -- dims the background color of inactive window
+                    enabled = false,            -- dims the background color of inactive window
                     shade = "dark",
-                    percentage = 0.15, -- percentage of the shade to apply to the inactive window
+                    percentage = 0.15,          -- percentage of the shade to apply to the inactive window
                 },
-                no_italic = false, -- Force no italic
-                no_bold = false, -- Force no bold
-                no_underline = false, -- Force no underline
-                styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-                    comments = { "italic" }, -- Change the style of comments
+                no_italic = false,              -- Force no italic
+                no_bold = false,                -- Force no bold
+                no_underline = false,           -- Force no underline
+                styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
+                    comments = { "italic" },    -- Change the style of comments
                     conditionals = { "italic" },
                     loops = {},
                     functions = {},
@@ -94,34 +93,33 @@ return {
     {
         "rebelot/kanagawa.nvim",
         name = "kanagawa",
-        config = function ()
+        config = function()
             require('kanagawa').setup({
-                compile = false,             -- enable compiling the colorscheme
-                undercurl = true,            -- enable undercurls
+                compile = false,  -- enable compiling the colorscheme
+                undercurl = true, -- enable undercurls
                 commentStyle = { italic = true },
                 functionStyle = {},
-                keywordStyle = { italic = true},
+                keywordStyle = { italic = true },
                 statementStyle = { bold = true },
                 typeStyle = {},
-                transparent = false,         -- do not set background color
-                dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
-                terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-                colors = {                   -- add/modify theme and palette colors
+                transparent = false,   -- do not set background color
+                dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+                terminalColors = true, -- define vim.g.terminal_color_{0,17}
+                colors = {             -- add/modify theme and palette colors
                     palette = {},
                     theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
                 },
                 overrides = function(colors) -- add/modify highlights
                     return {}
                 end,
-                theme = "wave",              -- Load "wave" theme when 'background' option is not set
-                background = {               -- map the value of 'background' option to a theme
-                    dark = "wave",           -- try "dragon" !
+                theme = "wave",    -- Load "wave" theme when 'background' option is not set
+                background = {     -- map the value of 'background' option to a theme
+                    dark = "wave", -- try "dragon" !
                     light = "lotus"
                 },
             })
 
             -- ColorMyPencils("kanagawa")
-
         end
     },
 
@@ -132,6 +130,15 @@ return {
             require('atomic-theme').setup()
 
             -- ColorMyPencils("atomic-theme")
+        end
+    },
+
+    {
+        "blazkowolf/gruber-darker.nvim",
+        name = "gruber-darker",
+        config = function()
+            require("gruber-darker").setup()
+            ColorMyPencils("gruber-darker")
         end
     },
 }
