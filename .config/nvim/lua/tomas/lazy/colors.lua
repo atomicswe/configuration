c230841fbf1d149efe5701f0286712af19762632
atomicswe <tomasbantunes@gmail.com>
2026-04-29 11:14:33 +0100

    everfores

--- .config/nvim/lua/tomas/lazy/colors.lua
+++ .config/nvim/lua/tomas/lazy/colors.lua
@@ -91,39 +91,6 @@ return {
     },
 
     {
-        "rebelot/kanagawa.nvim",
-        name = "kanagawa",
-        config = function()
-            require('kanagawa').setup({
-                compile = false,  -- enable compiling the colorscheme
-                undercurl = true, -- enable undercurls
-                commentStyle = { italic = true },
-                functionStyle = {},
-                keywordStyle = { italic = true },
-                statementStyle = { bold = true },
-                typeStyle = {},
-                transparent = false,   -- do not set background color
-                dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
-                terminalColors = true, -- define vim.g.terminal_color_{0,17}
-                colors = {             -- add/modify theme and palette colors
-                    palette = {},
-                    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-                },
-                overrides = function(colors) -- add/modify highlights
-                    return {}
-                end,
-                theme = "wave",    -- Load "wave" theme when 'background' option is not set
-                background = {     -- map the value of 'background' option to a theme
-                    dark = "wave", -- try "dragon" !
-                    light = "lotus"
-                },
-            })
-
-            -- ColorMyPencils("kanagawa")
-        end
-    },
-
-    {
         "atomicswe/atomic-theme",
         name = "atomic-theme",
         config = function()
@@ -143,14 +110,14 @@ return {
     },
 
     {
-        'sainnhe/everforest',
+        "sainnhe/everforest",
+        name = "everforest",
+        version = false,
         lazy = false,
         priority = 1000,
         config = function()
-            -- Optionally configure and load the colorscheme
-            -- directly inside the plugin declaration.
-            -- vim.g.everforest_enable_italic = true
+            vim.g.everforest_background = "hard" -- "soft", "medium", or "hard"
             -- ColorMyPencils("everforest")
         end
-    }
+    },
 }
