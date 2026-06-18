f572be4cb4c07b29cb7b6170593b8e556c7dc4f2
atomicswe <tomasbantunes@gmail.com>
2026-06-18 11:47:26 +0100

    some nice keybinds

--- .emacs.rc/misc-rc.el
+++ .emacs.rc/misc-rc.el
@@ -26,3 +26,35 @@
 (add-hook 'compilation-filter-hook 'rc/colorize-compilation-buffer)
 
 (global-set-key (kbd "C-c c") #'compile)
+
+(defun rc/duplicate-line ()
+  "Duplicate current line"
+  (interactive)
+  (let ((column (- (point) (point-at-bol)))
+        (line (let ((s (thing-at-point 'line t)))
+                (if s (string-remove-suffix "\n" s) ""))))
+    (move-end-of-line 1)
+    (newline)
+    (insert line)
+    (move-beginning-of-line 1)
+    (forward-char column)))
+
+(global-set-key (kbd "C-,") 'rc/duplicate-line)
+
+(defun rc/insert-timestamp ()
+  (interactive)
+  (insert (format-time-string "(%Y%m%d-%H%M%S)" nil t)))
+
+(global-set-key (kbd "C-x p d") 'rc/insert-timestamp)
+
+(defun rc/rgrep-selected (beg end)
+  (interactive (if (use-region-p)
+                   (list (region-beginning) (region-end))
+                 (list (point-min) (point-min))))
+  (rgrep (buffer-substring-no-properties beg end) "*" (pwd)))
+
+(global-set-key (kbd "C-x p s") 'rc/rgrep-selected)
+
+(setq confirm-kill-emacs 'y-or-n-p)
+
+(windmove-default-keybindings)
